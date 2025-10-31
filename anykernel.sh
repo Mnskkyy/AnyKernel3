e4fabbfa4eb03ea0680c22ba567fb90b175fb843
Mnskkyy <mansur180705@gmail.com>
2025-10-31 03:38:02 +0000

    hello World

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=MnzzKernel by @Mnskkyy
+kernel.string=MnzzKernel+ by @Mnskkyy
 do.devicecheck=1
 do.modules=0
 do.systemless=1
